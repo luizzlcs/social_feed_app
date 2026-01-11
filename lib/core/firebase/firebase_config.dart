746aea1b1c812e1378ed90a8530ce087b5428976
Luiz Carlos <luizzlcs@gmail.com>
2026-01-11 13:30:25 -0300

    Configuração do Firebase

---
+++ lib/core/firebase/firebase_config.dart
@@ -0,0 +1,11 @@
+class FirebaseConfig {
+  // Configurações do Firebase Web
+  static const webConfig = {
+    "apiKey": "AIzaSyBZTfJfQK4O3GtLNwCIJRhp4SyctVYhuEI",
+    "authDomain": "seven-manager-cffc7.firebaseapp.com",
+    "projectId": "seven-manager-cffc7",
+    "storageBucket": "seven-manager-cffc7.appspot.com",
+    "messagingSenderId": "345781640769",
+    "appId": "1:345781640769:web:7b268d566258692ee9f0c0",
+  };
+}
